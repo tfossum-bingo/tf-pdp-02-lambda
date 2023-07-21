0f08b084f83bfac9b99f407e1c1a065acda06730
Ted Fossum <ted.fossum@slalom.com>
2023-07-21 10:33:51 -0400

    Update message.

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -16,7 +16,7 @@ export const lambdaHandler = async (event, context) => {
         return {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'You star, you. You rule the world!',
             })
         }
     } catch (err) {
