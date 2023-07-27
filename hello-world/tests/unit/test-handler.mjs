ec9493e172ff5957b75109b8df33825ecc214ee6
Ted Fossum <ted.fossum@slalom.com>
2023-07-27 15:15:13 -0400

    Add some basic tasks for staging

--- hello-world/tests/unit/test-handler.mjs
+++ hello-world/tests/unit/test-handler.mjs
@@ -15,6 +15,6 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("You are my sunshine. You rule the world!");
+        expect(response.message).to.be.equal("You are thy staging sunshine. You rule the world!");
     });
 });
