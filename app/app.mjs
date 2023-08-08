98841639325d5950c603ffde27232c03c8ae02e7
Ted Fossum <ted.fossum@slalom.com>
2023-08-08 15:27:32 -0400

    Hardcoding and re-referencing

--- app/app.mjs
+++ app/app.mjs
@@ -16,7 +16,7 @@ export const lambdaHandler = async (event, context) => {
         return {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: `You are thy staging sunshine. You rule ${process.env.ENV_NAME}!`,
+                message: `You are thy staging sunshine. You rule ${process.env.APP_ENV_NAME}!`,
             })
         }
     } catch (err) {
